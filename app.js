ba2aeddf0f39adce52740565e5f630faedfafaee
Abhay kumar <abhaykumardev001@gmail.com>
2026-07-19 14:18:18 +0530

    Connected Monggose and set up path for views directiory

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
 
