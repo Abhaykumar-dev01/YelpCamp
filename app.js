e20525547874c3fb9dbbf121df4843ae06fc9db6
Abhay kumar <abhaykumardev001@gmail.com>
2026-07-19 23:18:41 +0530

    Add a /campground/new route for rendering a form to create new campground

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 })
 
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new');
+})
+
+
+
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
